94e08a77cf18eefbf1ac96de1c8244810a5fa412
moon <nowstart2691@lguplus.co.kr>
2024-08-06 13:04:43 +0900

    test(v0.0.2): 로드밸런싱 테스트

--- src/main/java/org/nowstart/appservice/controller/TestController.java
+++ src/main/java/org/nowstart/appservice/controller/TestController.java
@@ -4,11 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static java.lang.Thread.sleep;
+
 @RestController
 public class TestController {
 
     @GetMapping("/test")
-    public String test(HttpServletRequest request) {
+    public String test(HttpServletRequest request) throws InterruptedException {
+        sleep(1000);
         return request.getLocalAddr() + ":" + request.getServerPort();
     }
 }
