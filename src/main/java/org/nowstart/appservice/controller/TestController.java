8cacd1a1f3c24b12b1031f194876d372e5322f34
moon <nowstart2691@gmail.com>
2024-08-07 11:48:53 +0900

    feat(v0.1.0): swagger-ui 추가

--- src/main/java/org/nowstart/appservice/controller/TestController.java
+++ src/main/java/org/nowstart/appservice/controller/TestController.java
@@ -11,7 +11,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test(HttpServletRequest request) throws InterruptedException {
-        sleep(1000);
         return request.getLocalAddr() + ":" + request.getServerPort();
     }
 }
